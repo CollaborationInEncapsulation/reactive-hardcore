b2909a4bd3fd01b491e8ea28ff6a54ec2978279b
Oleh Dokuka <shadowgun@i.ua>
2018-10-20 01:35:26 +0300

    final

--- src/main/java/org/test/reactive/ArrayPublisher.java
+++ src/main/java/org/test/reactive/ArrayPublisher.java
@@ -1,7 +1,13 @@
 package org.test.reactive;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicLongFieldUpdater;
+
 import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 
 /**
  * Create Publisher that sends elements of a given array to each new subscriber
@@ -22,8 +28,143 @@ public class ArrayPublisher<T> implements Publisher<T> {
 
     @Override
     public void subscribe(Subscriber<? super T> subscriber) {
-        for (int i = 0; i < array.length; i++) {
-            subscriber.onNext(array[i]);
+        subscriber.onSubscribe(new ArraySubscription<>(array, subscriber));
+
+    }
+
+    private static class ArraySubscription<T> implements Subscription {
+
+        private final Subscriber<? super T> subscriber;
+
+        private final T[] array;
+
+        int index;
+
+        volatile boolean canceled;
+
+        volatile long requested;
+        static final AtomicLongFieldUpdater<ArraySubscription> REQUESTED =
+                AtomicLongFieldUpdater.newUpdater(ArraySubscription.class, "requested");
+
+        public ArraySubscription(T[] array, Subscriber<? super T> subscriber) {
+            this.array = array;
+            this.subscriber = subscriber;
+        }
+
+
+        @Override
+        public void request(long n) {
+
+            if (n <= 0) {
+                return;
+            }
+
+            long initialRequested;
+
+            do {
+                initialRequested = requested;
+
+                if (initialRequested == Long.MAX_VALUE) {
+                    return;
+                }
+
+                n = initialRequested + n;
+
+                if (n <= 0) {
+                    n = Long.MAX_VALUE;
+                }
+
+            } while (!REQUESTED.weakCompareAndSet(this, initialRequested, n));
+
+            if (initialRequested > 0) {
+                return;
+            }
+
+            if (n > (array.length - index)) {
+                fastPath();
+            } else {
+                slowPath(n);
+            }
+
+        }
+
+        void slowPath(long n) {
+            int sent = 0;
+            int idx = this.index;
+            T[] array = this.array;
+            int length = array.length;
+            Subscriber<? super T> subscriber = this.subscriber;
+
+            while (true) {
+                for (; sent < n && idx < length; sent++, idx++) {
+                    if (canceled) {
+                        return;
+                    }
+
+                    T element = array[idx];
+
+                    if (element == null) {
+                        subscriber.onError(new NullPointerException());
+                        return;
+                    }
+
+                    subscriber.onNext(element);
+                }
+
+                if (canceled) {
+                    return;
+                }
+
+                if (idx == length) {
+                    subscriber.onComplete();
+                    return;
+                }
+
+                n = requested;
+
+                if (n == sent) {
+                    index = idx;
+                    n = REQUESTED.addAndGet(this, -sent);
+                    if (n == 0) {
+                        return;
+                    }
+                }
+
+                sent = 0;
+            }
+        }
+
+        void fastPath() {
+            int idx = this.index;
+            T[] array = this.array;
+            int length = array.length;
+            Subscriber<? super T> subscriber = this.subscriber;
+
+            for (; idx < length; idx++) {
+                if (canceled) {
+                    return;
+                }
+
+                T element = array[idx];
+
+                if (element == null) {
+                    subscriber.onError(new NullPointerException());
+                    return;
+                }
+
+                subscriber.onNext(element);
+            }
+
+            if (canceled) {
+                return;
+            }
+
+            subscriber.onComplete();
+        }
+
+        @Override
+        public void cancel() {
+            canceled = true;
         }
     }
 }
