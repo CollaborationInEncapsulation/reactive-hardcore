d365ae1e6a39ab5f55c9cb958a0f3b0ef6481beb
Oleh Dokuka <shadowgun@i.ua>
2018-10-19 23:12:27 +0300

    Part 1. Initial

---
+++ src/jmh/java/org/test/reactive/ArrayPublisherPerfTest.java
@@ -0,0 +1,63 @@
+package org.test.reactive;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.RunnerException;
+
+@BenchmarkMode(Mode.Throughput)
+@Warmup(iterations = 2)
+@Measurement(iterations = 3, time = 3, timeUnit = TimeUnit.SECONDS)
+@OutputTimeUnit(TimeUnit.SECONDS)
+@Fork(value = 1)
+@State(Scope.Thread)
+public class ArrayPublisherPerfTest {
+    @Param({ "1000000" })
+    public int times;
+
+//    UnoptimizedArrayPublisher<Integer> unoptimizedArrayPublisher;
+    ArrayPublisher<Integer> arrayPublisher;
+
+    @Setup
+    public void setup() {
+        Integer[] array = new Integer[times];
+        Arrays.fill(array, 777);
+//        unoptimizedArrayPublisher = new UnoptimizedArrayPublisher<>(array);
+        arrayPublisher = new ArrayPublisher<>(array);
+    }
+
+    @Benchmark
+    public Object publisherPerformance(Blackhole bh) {
+        PerfSubscriber lo = new PerfSubscriber(bh);
+
+        arrayPublisher.subscribe(lo);
+
+        return lo;
+    }
+
+    @Benchmark
+    public Object unoptimizedPublisherPerformance(Blackhole bh) {
+        PerfSubscriber lo = new PerfSubscriber(bh);
+
+//        unoptimizedArrayPublisher.subscribe(lo);
+
+        return lo;
+    }
+
+    public static void main(String[] args) throws IOException, RunnerException {
+        org.openjdk.jmh.Main.main(args);
+    }
+}
---
+++ src/jmh/java/org/test/reactive/PerfSubscriber.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2017 David Karnok
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.test.reactive;
+
+import org.openjdk.jmh.infra.Blackhole;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+public final class PerfSubscriber implements Subscriber<Object> {
+    final  Blackhole      bh;
+
+    Subscription subscription;
+
+    public PerfSubscriber(Blackhole bh) {
+        this.bh = bh;
+    }
+
+    @Override
+    public void onSubscribe(Subscription subscription) {
+        this.subscription = subscription;
+        subscription.request(1);
+    }
+
+    @Override
+    public void onNext(Object item) {
+        bh.consume(item);
+        subscription.request(1);
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        bh.consume(throwable);
+    }
+
+    @Override
+    public void onComplete() {
+        bh.consume(true);
+    }
+
+}
