45155c9545b38afdbee8d138489fb6d0cd9c2d31
Oleh Dokuka <shadowgun@i.ua>
2019-03-27 23:23:35 +0100

    Step 1. Order test and solution

--- src/test/java/org/test/reactive/ArrayPublisherTest.java
+++ src/test/java/org/test/reactive/ArrayPublisherTest.java
@@ -1,9 +1,11 @@
 package org.test.reactive;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.LongStream;
 
@@ -20,6 +22,49 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.testng.Assert.assertEquals;
 
 public class ArrayPublisherTest {
+
+    @Test
+    public void signalsShouldBeEmittedInTheRightOrder() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        ArrayList<Long> collected = new ArrayList<>();
+        ArrayList<Integer> order = new ArrayList<>();
+        long toRequest = 5L;
+        Long[] array = generate(toRequest);
+        ArrayPublisher<Long> publisher = new ArrayPublisher<>(array);
+
+        publisher.subscribe(new Subscriber<>() {
+            @Override
+            public void onSubscribe(Subscription s) {
+                order.add(0);
+                s.request(toRequest);
+            }
+
+            @Override
+            public void onNext(Long aLong) {
+                collected.add(aLong);
+
+                if (!order.contains(1)) {
+                    order.add(1);
+                }
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onComplete() {
+                order.add(2);
+                latch.countDown();
+            }
+        });
+
+        latch.await(1, TimeUnit.SECONDS);
+
+        Assert.assertEquals(order, Arrays.asList(0, 1, 2));
+        Assert.assertEquals(collected, Arrays.asList(array));
+    }
 
     static Long[] generate(long num) {
         return LongStream.range(0, num >= Integer.MAX_VALUE ? 1000000 : num)
